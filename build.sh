b1c38654e88cb624ad4308f04df50414130f7bc4
Marcin Dulak <marcin.dulak@gmail.com>
2017-04-01 23:51:20 +0200

    need to refresh the local repo; yum clean all --enablerepo=minishift --disablerepo='*' refreshes all repos

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@ yum -y install "compiler(go-compiler)"
 spectool -g -R golang-github-asaskevich-govalidator/golang-github-asaskevich-govalidator.spec
 rpmbuild -bb golang-github-asaskevich-govalidator/golang-github-asaskevich-govalidator.spec
 
-createrepo ~/rpmbuild/RPMS
+createrepo ~/rpmbuild/RPMS&& yum clean all --enablerepo=minishift --disablerepo='*'
 yum -y install "golang(github.com/Masterminds/semver)"
 yum -y install "golang(github.com/dgrijalva/jwt-go)"
 yum -y install "golang(github.com/stretchr/testify/require)"
 spectool -g -R golang-github-Azure-go-autorest/golang-github-Azure-go-autorest.spec
 rpmbuild -bb golang-github-Azure-go-autorest/golang-github-Azure-go-autorest.spec
 
-createrepo ~/rpmbuild/RPMS
+createrepo ~/rpmbuild/RPMS&& yum clean all --enablerepo=minishift --disablerepo='*'
 yum -y install "golang(github.com/go-sql-driver/mysql)"
 spectool -g -R golang-github-DATA-DOG-go-txdb/golang-github-DATA-DOG-go-txdb.spec
 rpmbuild -bb golang-github-DATA-DOG-go-txdb/golang-github-DATA-DOG-go-txdb.spec
 
-createrepo ~/rpmbuild/RPMS
+createrepo ~/rpmbuild/RPMS&& yum clean all --enablerepo=minishift --disablerepo='*'
 yum -y install "golang(github.com/DATA-DOG/go-txdb)"
 spectool -g -R golang-github-DATA-DOG-godog/golang-github-DATA-DOG-godog.spec
 rpmbuild -bb golang-github-DATA-DOG-godog/golang-github-DATA-DOG-godog.spec
@@ -28,7 +28,7 @@ rpmbuild -bb golang-github-pkg-browser/golang-github-pkg-browser.spec
 spectool -g -R golang-github-pyr-egoscale/golang-github-pyr-egoscale.spec
 rpmbuild -bb golang-github-pyr-egoscale/golang-github-pyr-egoscale.spec
 
-createrepo ~/rpmbuild/RPMS
+createrepo ~/rpmbuild/RPMS&& yum clean all --enablerepo=minishift --disablerepo='*'
 yum -y install "golang(github.com/davecgh/go-spew/spew)"
 spectool -g -R golang-github-vmware-govmomi/golang-github-vmware-govmomi.spec
 rpmbuild -bb golang-github-vmware-govmomi/golang-github-vmware-govmomi.spec
@@ -36,7 +36,7 @@ rpmbuild -bb golang-github-vmware-govmomi/golang-github-vmware-govmomi.spec
 spectool -g -R golang-github-samalba-dockerclient/golang-github-samalba-dockerclient.spec
 rpmbuild -bb golang-github-samalba-dockerclient/golang-github-samalba-dockerclient.spec
 
-createrepo ~/rpmbuild/RPMS
+createrepo ~/rpmbuild/RPMS&& yum clean all --enablerepo=minishift --disablerepo='*'
 yum -y install "golang(github.com/Azure/azure-sdk-for-go/storage)"
 yum -y install "golang(github.com/Azure/go-autorest/autorest)"
 yum -y install "golang(github.com/aws/aws-sdk-go/aws)"
@@ -55,11 +55,13 @@ yum -y install "golang(golang.org/x/oauth2)"
 spectool -g -R golang-github-docker-machine/golang-github-docker-machine.spec
 rpmbuild -bb golang-github-docker-machine/golang-github-docker-machine.spec
 
+createrepo ~/rpmbuild/RPMS&& yum clean all --enablerepo=minishift --disablerepo='*'
 yum -y install "golang(github.com/DATA-DOG/godog)"
 yum -y install "golang(github.com/asaskevich/govalidator)"
 yum -y install "golang(github.com/blang/semver)"
 yum -y install "golang(github.com/docker/machine/libmachine)"
 yum -y install "golang(github.com/google/go-github/github)"  # https://bugzilla.redhat.com/show_bug.cgi?id=1430132
+yum -y install "golang(github.com/inconshreveable/go-update)"
 yum -y install "golang(github.com/kardianos/osext)"
 yum -y install "golang(github.com/olekukonko/tablewriter)"
 yum -y install "golang(github.com/pborman/uuid)"
